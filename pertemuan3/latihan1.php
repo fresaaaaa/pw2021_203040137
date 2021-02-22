2e381f137cb4246ab9059fead2bab3188b6d786b
fresa.ba <76567424+fresaaaaa@users.noreply.github.com>
2021-02-23 04:21:57 +0700

    menyelesaikan video pertemuan3

---
+++ pertemuan3/latihan1.php
@@ -0,0 +1,32 @@
+<?php
+/* 
+    Pertemuan 3
+    Fresabayu Anggoro
+    203040137
+    https://github.com/fresaaaaa/pw2021_203040137
+    19 - 02 - 2021
+    Struktur Kendali PHP
+ */
+ ?>
+
+ <?php 
+    // Pengulangan
+    // for
+    // while
+    // do.. while
+    // foreach : pengulangan khusus array
+
+    // for( $i = 0; $i < 5; $i++ ) {
+    //     echo "Hello World! <br>";
+    // }
+    // $i = 0;
+    // while( $i < 5 ) {
+    //     echo "Hello World! <br>";
+    //     $i++;
+    // }
+    // $i = 0;
+    // do {
+    //     echo "Hello World! <br>";
+    //     $i++;
+    // } while( $i < 5 );
+ ?>
