1993f3085ad7efe90f1c4064f1b0674b4de39fda
fresa.ba <76567424+fresaaaaa@users.noreply.github.com>
2021-03-03 01:04:46 +0700

    Latihan dan Tugas Praktikum Pemrograman Web

---
+++ praktikum/tugas1/latihan1a.php
@@ -0,0 +1,13 @@
+<?php
+/*
+    Fresabayu Anggoro
+    203040137
+    Jumat 13.00, 26 Februari 2021
+    latihan 1A Praktikum PW
+    https://github.com/fresaaaaa/pw2021_203040137
+*/
+   for( $i = 1; $i <= 3; $i++ )
+        for ($j = 1; $j <= 3; $j++) {
+        echo "Ini perulangan ke ($i, $j) <br>";
+    }
+?>
