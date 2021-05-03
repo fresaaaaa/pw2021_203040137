acd07f67b59e67a767c0aa168840d679c39b30ed
fresaaaaa <fresabayu0601@gmail.com>
2021-05-04 01:52:42 +0700

    menambahkan fungsi ubah, tambah dan pencarian

---
+++ pertemuan11/hapus.php
@@ -0,0 +1,17 @@
+<?php
+require 'function.php';
+
+// mengambil id dari url
+$id = $_GET['id'];
+
+if (hapus($id) > 0) {
+  echo "<script>
+          alert('Data berhasil dihapus');
+          document.location.href = 'index.php';
+        </script>";
+} else {
+  echo "<script>
+           alert('Data gagal dihapus');
+            document.location.href = 'index.php';
+        </script>";
+}
